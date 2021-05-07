e1b9fb8fe1e7a058998655ce23a8737f5acb76df
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-07 11:39:42 +0200

    My first class Department

---
+++ codes/classes/index.ts
@@ -0,0 +1,10 @@
+class Department {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+var myDepartment = new Department("Kizihira");
+console.log(myDepartment.name);
---
+++ out/codes/classes/index.js
@@ -0,0 +1,8 @@
+"use strict";
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+}
+var myDepartment = new Department("Kizihira");
+console.log(myDepartment.name);

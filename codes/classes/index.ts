096494d9c8dfa35ff965a091ab0f4cd58674a4bf
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-07 12:13:00 +0200

    Add Employees

--- codes/classes/index.ts
+++ codes/classes/index.ts
@@ -1,10 +1,30 @@
 class Department {
     name: string;
+    employees: string[] = [];
 
     constructor(name: string) {
         this.name = name;
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeesInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+
+    describe() {
+        console.log("Department : " + this.name);
+    }
 }
 
-var myDepartment = new Department("Kizihira");
-console.log(myDepartment.name);
+var myDepartment = new Department("Humman Resource");
+myDepartment.describe();
+
+myDepartment.addEmployee("Kalisa");
+myDepartment.addEmployee("Augustin");
+myDepartment.addEmployee("Rugaba");
+
+myDepartment.printEmployeesInformation();
--- out/codes/classes/index.js
+++ out/codes/classes/index.js
@@ -1,8 +1,23 @@
 "use strict";
 class Department {
     constructor(name) {
+        this.employees = [];
         this.name = name;
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeesInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+    describe() {
+        console.log("Department : " + this.name);
+    }
 }
-var myDepartment = new Department("Kizihira");
-console.log(myDepartment.name);
+var myDepartment = new Department("Humman Resource");
+myDepartment.describe();
+myDepartment.addEmployee("Kalisa");
+myDepartment.addEmployee("Augustin");
+myDepartment.addEmployee("Rugaba");
+myDepartment.printEmployeesInformation();
